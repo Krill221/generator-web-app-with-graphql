7586478bc51f70c1b59094c1f595928ce19ed426
kirilleropkin <krill221@gmail.com>
2020-06-24 16:09:29 +0400

    added devider to createManyList

--- generators/app/templates/src/layouts/PublicMainLayout.js
+++ generators/app/templates/src/layouts/PublicMainLayout.js
@@ -10,15 +10,19 @@ const Layout = ({ component: Component, ...rest }) => {
         <Route
             {...rest}
             render={(props) =>
-                    <React.Fragment>
-                        <AppBar position="sticky">
-                            <Toolbar id="menu">
-                                <Button color="inherit" onClick={() => history.push('/')} id='signout'>Hotels</Button>
-                                <Typography style={{flexGrow: 1}} variant="h5" noWrap />
-                                <ProfileButtons />
-                            </Toolbar>
-                        </AppBar>
+                <React.Fragment>
+                    <AppBar position="sticky">
+                        <Toolbar id="menu">
+                            <Button color="inherit" onClick={() => history.push('/')} id='signout'>Hotels</Button>
+                            <Typography style={{ flexGrow: 1 }} variant="h5" noWrap />
+                            <ProfileButtons />
+                        </Toolbar>
+                    </AppBar>
                     <Component {...props} />
+                    <footer>
+                        <Typography variant="h6" align="center" gutterBottom> O-Hotel </Typography>
+                        <Typography variant="subtitle2" align="center" color="textSecondary" component="p">© {new Date().getFullYear()} One Hotel, Inc. All rights reserved</Typography>
+                    </footer>
                 </React.Fragment>
             }
         />
