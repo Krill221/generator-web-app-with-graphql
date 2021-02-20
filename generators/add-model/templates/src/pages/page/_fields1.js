8746f3b002470786c882162cfeaf39711813f546
kirilleropkin <krill221@gmail.com>
2021-02-20 14:12:24 +0300

    update

--- generators/add-model/templates/src/pages/page/_fields1.js
+++ generators/add-model/templates/src/pages/page/_fields1.js
@@ -1,10 +1,14 @@
 
 import React from 'react';
 import { TextField, Grid, Container } from '@material-ui/core';
+//import { InputAdornment, IconButton } from '@material-ui/core';
+//import SendIcon from '@material-ui/icons/Send';
 //import { useTheme } from '@material-ui/core/styles';
 
 export default function EditView1(props) {
+    
     // const theme = useTheme();
+
     return <Container>
         <Grid container spacing={2} justify="center" alignItems="center">
             <% fields.forEach(function(field){ %><Grid item xs={12} sm={12} md={12} >
@@ -20,6 +24,15 @@ export default function EditView1(props) {
                     helperText={props.errors.<%= field[0] %> && props.touched.<%= field[0] %> ? props.errors.<%= field[0] %> : null}
                     onBlur={e => { props.handleChange(e); props.submitForm(); }}
                     onChange={props.handleChange}
+                    onKeyDown={e => { /*if (e.key === 'Enter') { props.submitForm(); } */ }}
+                    InputProps={{
+                        /*endAdornment: <InputAdornment position="end">
+                            <IconButton edge="end" className='add-button' onClick={e => { props.submitForm(); }} aria-label="search">
+                                <SendIcon />
+                            </IconButton>
+                        </InputAdornment>,
+                        */
+                    }}
                 />
             </Grid>
             <% }) %>
