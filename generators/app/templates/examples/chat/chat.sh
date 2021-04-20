8640ae7c64fb67b143165886ee4aea6ac219696b
kirilleropkin <krill221@gmail.com>
2021-04-20 19:05:43 +0300

    bugs fixed

--- generators/app/templates/examples/chat/chat.sh
+++ generators/app/templates/examples/chat/chat.sh
@@ -5,5 +5,11 @@ yarn add-has-many Chat Message
 yarn add-has-many User Message
 
 # 2. LIKES (add to model)
-yarn add-model Like value:String userId:ID
+yarn add-model Like value:String
+yarn add-has-many User Like
 yarn add-has-estime Message Like
+
+# 3. Copy pages to /src/pages
+
+# add to Menu.js
+# chats
